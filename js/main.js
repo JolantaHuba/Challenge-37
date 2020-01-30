9ee02c2f97bbed5e4cb50221a887fefeba866f6a
JolantaHuba <huba.jolanta@gmail.com>
2020-01-30 21:22:32 +0100

    Add mobile nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 "use strict"
+
+const burgerBtn = document.querySelector('.nav__burger');
+const navList = document.querySelector('.nav__list');
+const navOverlay = document.querySelector('.nav__overlay');
+
+const toggleMenu = () => {
+    burgerBtn.classList.toggle('active');
+    navList.classList.toggle('active');
+    navOverlay.classList.toggle('active');
+}
+
+// const hideMenu = () => {
+//     burgerBtn.classList.remove('active');
+//     navList.classList.remove('active');
+//     navOverlay.classList.remove('active');
+// }
+
+const hideMenu = () => {
+    const activeMenuElements = document.querySelectorAll('nav .active');
+    activeMenuElements.forEach(element => element.classList.remove('active'));
+}
+
+burgerBtn.addEventListener('click', toggleMenu);
+navOverlay.addEventListener('click', hideMenu);
+navList.addEventListener('click', hideMenu);
