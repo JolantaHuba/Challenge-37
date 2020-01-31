cd52cf70ea34b8c76aed2f323d0631bb327d5a46
JolantaHuba <huba.jolanta@gmail.com>
2020-01-31 14:29:35 +0100

    Nav desktop and scroll animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 "use strict"
-
+const nav = document.querySelector('.nav');
 const burgerBtn = document.querySelector('.nav__burger');
 const navList = document.querySelector('.nav__list');
 const navOverlay = document.querySelector('.nav__overlay');
@@ -21,6 +21,13 @@ const hideMenu = () => {
     activeMenuElements.forEach(element => element.classList.remove('active'));
 }
 
+const scrollMenu = () => {
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else nav.classList.remove('scrolled');
+}
+
 burgerBtn.addEventListener('click', toggleMenu);
 navOverlay.addEventListener('click', hideMenu);
 navList.addEventListener('click', hideMenu);
+document.addEventListener('scroll', scrollMenu);
