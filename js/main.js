446baf755edf2d44744f8efbd3063660db85399d
JolantaHuba <huba.jolanta@gmail.com>
2020-02-17 21:54:21 +0100

    Clients section - JS change (quotes from json using ajax)

--- js/main.js
+++ js/main.js
@@ -28,60 +28,61 @@ navList.addEventListener('click', hideMenu);
 document.addEventListener('scroll', scrollMenu);
 
 //current section in menu
-const home = document.querySelector('.home')
+const home = document.querySelector('.home');
 const about = document.querySelector('.about');
 const services = document.querySelector('.services');
 const process = document.querySelector('.process');
 const team = document.querySelector('.team');
 const clients = document.querySelector('.clients');
 const contact = document.querySelector('.contact');
-
 const navLinks = document.querySelectorAll('.nav__link');
 
+
 const showCurrentSection = () => {
-    if (window.scrollY < 100) {
+    const scrollPos = window.scrollY;
+
+    if (scrollPos < 100) {
         navLinks.forEach(navLink => {
             navLink.classList.remove('current');
         });
-    } else if (window.scrollY < home.offsetHeight) {
+    } else if (scrollPos < home.offsetHeight) {
         navLinks.forEach(navLink => {
             if (navLink.dataset.name !== 'home') navLink.classList.remove('current');
         });
         document.querySelector('[data-link=home]').classList.add('current');
-    } else if (window.scrollY < about.offsetTop + about.offsetHeight - 10) {
+    } else if (scrollPos < about.offsetTop + about.offsetHeight - 10) {
         navLinks.forEach(navLink => {
             if (navLink.dataset.name !== 'about') navLink.classList.remove('current');
         });
         document.querySelector('[data-link=about]').classList.add('current');
-    } else if (window.scrollY < services.offsetTop + services.offsetHeight - 10) {
+    } else if (scrollPos < services.offsetTop + services.offsetHeight - 10) {
         navLinks.forEach(navLink => {
             if (navLink.dataset.name !== 'services') navLink.classList.remove('current');
         });
         document.querySelector('[data-link=services]').classList.add('current');
-    } else if (window.scrollY < process.offsetTop + process.offsetHeight - 10) {
+    } else if (scrollPos < process.offsetTop + process.offsetHeight - 10) {
         navLinks.forEach(navLink => {
             if (navLink.dataset.name !== 'process') navLink.classList.remove('current');
         });
         document.querySelector('[data-link=process]').classList.add('current');
-    } else if (window.scrollY < team.offsetTop + team.offsetHeight - 10) {
+    } else if (scrollPos < team.offsetTop + team.offsetHeight - 10) {
         navLinks.forEach(navLink => {
             if (navLink.dataset.name !== 'team') navLink.classList.remove('current');
         });
         document.querySelector('[data-link=team]').classList.add('current');
-    } else if (window.scrollY < clients.offsetTop + clients.offsetHeight - 10) {
+    } else if (scrollPos < clients.offsetTop + clients.offsetHeight - 10) {
         navLinks.forEach(navLink => {
             if (navLink.dataset.name !== 'clients') navLink.classList.remove('current');
         });
         document.querySelector('[data-link=clients]').classList.add('current');
     }
 
-    if (window.scrollY > document.body.clientHeight - window.innerHeight - 100) {
+    if (scrollPos > document.body.clientHeight - window.innerHeight - 100) {
         navLinks.forEach(navLink => {
             if (navLink.dataset.name !== 'contact') navLink.classList.remove('current');
         });
         document.querySelector('[data-link=contact]').classList.add('current');
     }
-
 }
 
 window.addEventListener('scroll', showCurrentSection)
@@ -91,7 +92,6 @@ window.addEventListener('scroll', showCurrentSection)
 const prevArrow = document.querySelector('.team__prev-arrow');
 const nextArrow = document.querySelector('.team__next-arrow');
 let carouselItems = [...document.querySelectorAll('.team__carousel-item')];
-
 
 const changeSlide = function () {
     const newCarousel = [];
@@ -121,40 +121,44 @@ const changeSlide = function () {
             item.className = 'team__carousel-item item--right';
         } else item.className = 'team__carousel-item item--hidden';
     });
-
-
-    // console.log(newCarousel);
 }
 
 carouselItems.forEach(item => {
     item.addEventListener('click', changeSlide);
 });
-
 prevArrow.addEventListener('click', changeSlide);
 nextArrow.addEventListener('click', changeSlide);
 
 // testimonials
 
-const quotes = [{
-        imgSrc: 'images/person4.jpg',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas praesentium magnam assumenda quae ipsa illo hic, id sunt eveniet at accusamus rerum autem unde.',
-        author: 'Jinny Snow, Company CEO',
-    },
-    {
-        imgSrc: 'images/person3.jpg',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas praesentium magnam assumenda quae ipsa illo hic.',
-        author: 'John Snow, Company CTO',
-    },
-    {
-        imgSrc: 'images/person1.jpg',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas praesentium magnam assumenda quae ipsa illo hic, id sunt eveniet at accusamus rerum autem unde. Quas praesentium magnam assumenda quae.',
-        author: 'Jenny Smith, Company CEO',
-    }
-];
-
-const thumbnails = document.querySelectorAll('.clients__img');
+const thumbnails = document.querySelectorAll('.clients__small-img');
 const mainImg = document.querySelector('.clients__main-img');
 const activeQuote = document.querySelector('.clients__quote');
+let quotes = null; // loaded from testimonials.json using ajax
+
+const loadTestimonials = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'js/testimonials.json', true);
+    xhr.onload = () => {
+        if (xhr.status == 200) {
+            const data = JSON.parse(xhr.responseText);
+            return quotes = data;
+        }
+    }
+    xhr.send();
+}
+
+const changeThumbnail = (activeImage) => {
+    thumbnails.forEach(thumbnail => {
+        if (thumbnail.parentNode.classList.contains('active')) {
+            thumbnail.parentNode.classList.remove('active');
+        }
+
+        if (thumbnail.dataset.author === activeImage.dataset.author) {
+            thumbnail.parentNode.classList.add('active');
+        }
+    });
+}
 
 const changeQuote = function () {
     const activeImage = this;
@@ -162,13 +166,16 @@ const changeQuote = function () {
     mainImg.classList.add('active');
     activeQuote.classList.add('active');
 
+    // Load quotes from JSON file
+    loadTestimonials();
+
     setTimeout(() => {
-        mainImg.src = activeImage.getAttribute('src');
         quotes.forEach(quote => {
-            if (quote.imgSrc === activeImage.getAttribute('src')) {
+            if (activeImage.dataset.author === quote.author) {
+                mainImg.src = quote.imgSrc;
                 activeQuote.innerHTML = `${quote.text}
                 <cite class="clients__quote-author">
-                ${quote.author}
+                ${quote.author}, ${quote.author_job}
                 </cite>`;
             }
         });
@@ -179,18 +186,10 @@ const changeQuote = function () {
         activeQuote.classList.remove('active');
     }, 400);
 
-
     // changeThumbnail
-    thumbnails.forEach(thumbnail => {
-        if (thumbnail.parentNode.classList.contains('active')) {
-            thumbnail.parentNode.classList.remove('active');
-        }
-
-        if (thumbnail.getAttribute('src') === activeImage.getAttribute('src')) {
-            thumbnail.parentNode.classList.add('active');
-        }
-    });
+    changeThumbnail(activeImage);
 }
+
 
 thumbnails.forEach(thumbnail => {
     thumbnail.addEventListener('click', changeQuote);
